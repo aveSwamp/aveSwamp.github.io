c28823f60b6d542debdc61b76d059d7f4467a776
s0nny13 <alexander.korsvold@gmail.com>
2026-02-28 16:03:57 +0300

    testing memory

---
+++ projects/simple_renderer/js/loader.js
@@ -0,0 +1,29 @@
+async function LOADMODULE(){
+    const responce = await fetch("../wasm-modules/add.wasm");
+    const bytes = await responce.arrayBuffer();
+    const module = await WebAssembly.compile(bytes);
+    const simple_renderer_instance = await WebAssembly.instantiate(module);
+    return simple_renderer_instance;
+}
+
+async function LOADSCENE(){
+    const responce = await fetch("../models/test_scene.obj");
+    const content = await responce.text();
+    return content
+}
+
+async function main(){
+    simple_renderer_instance = await LOADMODULE()
+    obj_scene_file = await LOADSCENE();
+    const memory = simple_renderer_instance.exports.memory;
+    const heap_base = simple_renderer_instance.exports.__heap_base;
+
+    console.log(typeof heap_base);
+    //const encoder = new TextEncoder();
+    //const objbytearray = encoder.encode(obj_scene_file);
+
+    //const view = new Uint8Array(memory.buffer);
+    //view.set(objbytearray, heap_base);
+}
+
+main();
